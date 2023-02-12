79f0b4e8dd914a289a9417623a32ec731bf4ce40
salimjon10 <salimjonrahimov10@mail.ru>
2023-02-12 20:02:23 +0700

    Решил первую и вторую задачу

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число a:");
+            int.TryParse(Console.ReadLine(), out int Number);
+
+            Console.Write("Введите число n:");
+            int.TryParse(Console.ReadLine(), out int Degree);
+
+            int Result = 1;
+
+            for (int Step = 0; Step < Degree; ++Step)
+            {
+                Result *= Number;
+            }
+
+            Console.WriteLine($"{Number}^{Degree} = {Result}");
+            
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.Write("Введите число:");
+
+            string Number = Console.ReadLine();
+
+            Number = (Number + Number[1]).Remove(1,1);
+
+            Console.WriteLine(Number);
+
+
+            /*String Number, NewNumber = "";
+
+            Console.Write("Введите число:");
+
+            Number = Console.ReadLine();
+
+            for (int Step = 0; Step < Number.Length; Step++)
+            {
+                if (Step == 1)
+                {
+                    continue;
+                }
+                NewNumber += Number[Step];
+            }
+
+            NewNumber += Number[1];
+
+            Console.WriteLine(NewNumber);*/
+        }
+    }
+}
